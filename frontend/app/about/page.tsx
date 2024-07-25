61a99154ae28e8e1d89bf4509189293faf5d6338
HAnthony007 <harifetraanthonyr@gmail.com>
2024-07-25 11:01:35 +0300

    Update

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,9 +1,11 @@
+import EmployeListe from "@/components/employe_list";
 import { title } from "@/components/primitives";
 
 export default function AboutPage() {
   return (
     <div>
       <h1 className={title()}>About</h1>
+      <EmployeListe />
     </div>
   );
 }
--- frontend/components/employe_list.tsx
+++ frontend/components/employe_list.tsx
@@ -1,24 +1,30 @@
 "use client"
 import useSWR from 'swr';
 import { Employe } from '../types/employe';
+import { useState } from 'react';
+import { fetcher } from '@/utils/api';
 // import { fetcher } from '@/utils/api';
 
-const fetcher = (url: string) => fetch(url).then(res => res.json());
 export default function EmployeListe() {
-    const { data: employes, error, isLoading } = useSWR<Employe[]>('http://localhost:8080/employe/all_employe', fetcher);
+    const [error, setError] = useState<string | null>(null);
+    const { data: employes, isLoading } = useSWR<Employe[]>('http://localhost:8080/employe/all_employe', fetcher, {
+        onError: (error) => {
+            setError(error.message || "An error occurred while fetching the data.");
+        }
+    });
 
     if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>Failed to load</div>;
-    if (!employes) return <div>No data</div>;
+    if (error) return <div>Failed to load: {error}</div>;
+    if (!employes || employes.length === 0) return <div>No data</div>;
     return (
-        <div>
+        <ul>
             {employes.map(employe => (
-                <div key={employe.n_matricule}>
-                    <h2>{employe.nom_empl}</h2>
+                <li key={employe.n_matricule}>
+                    <h2>{employe.nom_empl} {employe.prenom_empl}</h2>
                     <p>{employe.email_empl}</p>
                     <p>{employe.role}</p>
-                </div>
+                </li>
             ))}
-        </div>
+        </ul>
     )
 }
